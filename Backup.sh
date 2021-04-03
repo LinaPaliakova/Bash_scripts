50dbce3a61fb0b92a9d8953307c329f3e86c9401
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2021-04-03 06:07:36 -0400

    Update Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
 #This script is aimed to create a backup of folder. All actions performed by script are logged into backup_logs
+if [ -z "$1" ]; then
+                echo "You have failed to pass a parameter. Please try again."
+                        exit 255;
+                fi
+mylog=$1
+date=`date`
+function MyExit {
+rm -rf /home/$USER/work_backup
+rm -f /home/$USER/$mylog
+echo "Received Ctrl+C"
+ exit 255
+ }
 
-echo " Starting a script "  >> /home/cloud_user/backup_logs
-echo " Creating directory "  >> /home/cloud_user/backup_logs
-mkdir /home/cloud_user/work_backup >> /home/cloud_user/backup_logs
-echo " Creating a backup " >> /home/cloud_user/backup_logs
-cp -v /home/cloud_user/work/*  /home/cloud_user/work_backup >> /home/cloud_user/backup_logs
-echo " The script finished " >> /home/cloud_user/backup_logs
+trap MyExit SIGINT
+echo "Timestamp before work is done $date" >> /home/$USER/$mylog
+echo "Creating backup directory" >> /home/$USER/$mylog
+mkdir /home/$USER/work_backup
+
+echo "Copying Files" >> /home/$USER/$mylog
+cp -v /home/$USER/work/* /home/$USER/work_backup/ >> /home/$USER/$mylog
+echo "Finished Copying Files" >> /home/$USER/$mylog
+echo "Timestamp after work is done $date" >> /home/$USER/$MYLOG
+
+sleep 15
