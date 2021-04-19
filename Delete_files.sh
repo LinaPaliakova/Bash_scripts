78728a50a370a08a25b981e41327f137b534fa66
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2021-04-19 14:58:11 -0400

    Update Delete_files.sh

--- Delete_files.sh
+++ Delete_files.sh
@@ -1,22 +1,16 @@
 #!/bin/bash 
 
-# This script will check on txt files in home directory, delete them and send mail;
+# This script will check on txt files in provided directory and delete them;
 
-path=$HOME
-log-"./Desktop/log"
-echo "Checking on files"
-files=$(find $path -type f -name "*.txt" | wc -l)
+path=$1
+files=$(find $path -type f -name "*.txt" )
 
-if [ $files -gt 0 ];
-then
-find $path -type f -name "*.txt" -exec rm -f {} \;
-echo "TXT files were deleted" >> $log
-else
-echo " No TXT files found" >> $log
-fi
+for i in ${files[@]}; do
+echo "Deleting $i files"
+rm i
+done
 
-subject="Daily txt files check"
-to="email address"
 
-echo "Sending email"
-mail -s "$subject" "$to" < $log
+
+
+
