159ee8be4aac71d746093ce3465057cfe9ee4fb2
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2021-06-29 04:08:51 -0400

    Update Asteriskrestart.sh

--- Asteriskrestart.sh
+++ Asteriskrestart.sh
@@ -5,7 +5,7 @@
 
 # If asterisk was configured as a service, you can use  service asterisk restart. The service will be restarted immediately.
 
-if [ "$(whoami)" != "asterisk" ];
+if [ "$(whoami)" != "asterisk" ]; #optinal part
 then
 echo "Script must be running as user asterisk"
 exit 255
@@ -19,4 +19,7 @@ while ps ax | grep '[s]afe_asterisk' >/dev/null ; do sleep 3 ; done
 
 service asterisk start >/dev/null 2>&1                              
 echo "Asterisk restarted at `date`" | mail -s "Asterisk restarted" abc@gmail.com
+
+
+# core restart gracefully. These commands do not restart asterisk application: the PID is left as it was; the process /usr/sbin/asterisk is left running; the used memory is left under the process.
 
